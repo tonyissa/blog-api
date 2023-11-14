c1d1211a4b0bb8aaaf638217f6fecfc09c525b46
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-14 17:09:00 -0500

    Add boilerplate code for API controller

---
+++ controllers/apiController.js
@@ -0,0 +1,33 @@
+const asyncHandler = require("express-async-handler");
+
+exports.index_get = asyncHandler(async (req, res, next) => {
+    
+})
+
+exports.about_get = asyncHandler(async (req, res, next) => {
+    
+})
+
+exports.blog_get = asyncHandler(async (req, res, next) => {
+    
+})
+
+exports.blog_post = asyncHandler(async (req, res, next) => {
+    
+})
+
+exports.blog_edit = asyncHandler(async (req, res, next) => {
+    
+})
+
+exports.blog_delete = asyncHandler(async (req, res, next) => {
+    
+})
+
+exports.comment_post = asyncHandler(async (req, res, next) => {
+    
+})
+
+exports.comment_delete = asyncHandler(async (req, res, next) => {
+    
+})
