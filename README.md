7510776fbc315062638542fbae7221a5fff64f6a
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-14 14:11:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# blog-client
+Odin Project blog backend
---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
+
+var app = express();
+
+// mongoose setup
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI;
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+main().catch(err => console.log(err));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
+
+module.exports = app;
