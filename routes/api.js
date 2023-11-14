72e86947cbf1cd92f5b93b8d047df4e35a9c257f
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-14 17:04:30 -0500

    Add routes for processing by API

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const api_controller = require('../controllers/apiController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', api_controller.index_get);
+
+router.get('/about', api_controller.about_get);
+
+router.get('/blog/:id', api_controller.blog_get);
+
+router.post('/blog', api_controller.blog_create);
+
+router.put('/blog/:id', api_controller.blog_edit);
+
+router.post('/blog/:id/comment', api_controller.comment_create);
+
+router.delete('/blog/:id/comment', api_controller.comment_delete);
 
 module.exports = router;
