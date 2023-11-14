ba5b555d7cdd5547516205f00dcfaa089b141e7d
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-14 14:14:49 -0500

    Add comment model

---
+++ models/Comment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    username: { type: String, required: true },
+    comment: { type: String, required: true },
+    date: { type: Date, required: true }
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
