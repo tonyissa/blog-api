d81fd9a4209ca58b34cb969ee45509b80a2ce4bf
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-27 15:08:40 -0500

    Add fly config to repo

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const cors = require('cors');
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3001' }));
+app.use(cors({ credentials: true, origin: 'https://antwonthedamaja.github.io/odin-blog-and-portfolio/' }));
 
 app.use('/api', apiRouter);
 
